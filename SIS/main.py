cd3fe142dd9387df937ebd6b4458e6382bbd9d74
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-02-13 11:16:12 +0530

    Motor controller added.

--- SIS/main.py
+++ SIS/main.py
@@ -5,48 +5,51 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 from data_processor import load_dataset
+from utils import show_bunch_properties
 
-# Call the data processor method which returns a Bunch object
-raw_data_bunch = load_dataset()
 
-# print raw_data_bunch.keys()
-# print raw_data_bunch.data.shape
-# print raw_data_bunch.feature_names
-# print raw_data_bunch.DESCR
+if __name__ == '__main__':
+    raw_data_bunch = load_dataset()
 
-print '________________________________________________________________________'
+    show_bunch_properties(raw_data_bunch)
 
-# Converting raw data to pandas data frame
-data = pd.DataFrame(raw_data_bunch.data)
-data.columns = raw_data_bunch.feature_names
-print data.head()
 
-print '________________________________________________________________________'
 
-# Add the target values
-data['WATER'] = raw_data_bunch.target
+    # Converting raw data to pandas data frame
+    data = pd.DataFrame(raw_data_bunch.data)
+    data.columns = raw_data_bunch.feature_names
+    print data.head()
 
-# Remove target value from X
-X = data.drop('WATER', axis=1)
+    print '________________________________________________________________________'
 
-l_reg = LinearRegression(n_jobs=-1)
+    # Add the target values
+    data['WATER'] = raw_data_bunch.target
 
-l_reg.fit(X, data.WATER)
+    # Remove target value from X
+    X = data.drop('WATER', axis=1)
 
-print 'Estimated intercept coefficient:', l_reg.intercept_
-print 'Number of coefficients:', len(l_reg.coef_)
+    l_reg = LinearRegression(n_jobs=-1)
 
-print '________________________________________________________________________'
+    l_reg.fit(X, data.WATER)
 
-print pd.DataFrame(zip(X.columns, l_reg.coef_), columns=['features', 'estimatedCoefficients'])
+    print 'Estimated intercept coefficient:', l_reg.intercept_
+    print 'Number of coefficients:', len(l_reg.coef_)
 
-plt.scatter(data.CMOIST, data.WATER)
-plt.xlabel('Current moisture')
-plt.ylabel('Required water')
-plt.title('Relationship between current moisture and required water')
-plt.show()
+    print '________________________________________________________________________'
 
-print '________________________________________________________________________'
+    print pd.DataFrame(zip(X.columns, l_reg.coef_), columns=['features', 'estimatedCoefficients'])
 
-mseFull = np.mean((data.WATER - l_reg.predict(X)) ** 2)
-print mseFull
+    plt.scatter(data.CMOIST, data.WATER)
+    plt.xlabel('Current moisture')
+    plt.ylabel('Required water')
+    plt.title('Relationship between current moisture and required water')
+    plt.show()
+
+    print '________________________________________________________________________'
+
+    mseFull = np.mean((data.WATER - l_reg.predict(X)) ** 2)
+    print mseFull
+
+
+
+
---
+++ SIS/utils.py
@@ -0,0 +1,9 @@
+def show_bunch_properties(bunch):
+    print ''
+    print 'Bunch properties________________________________________________________'
+    print bunch.keys()
+    print bunch.data.shape
+    print bunch.feature_names
+    # print bunch.DESCR
+    print ''
+
