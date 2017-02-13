6206ae0575249023f316a1a7baffa5b900c6d0e0
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-02-14 00:35:07 +0530

    Create crop class

---
+++ FlowController/crop.py
@@ -0,0 +1,13 @@
+class Crop:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size			# Size of the crop field in sq.ft.
+        self.state = "ACTIVE"		# ACTIVE FINISHED CANCELLED
+        self.volume_to_supply = 0.00	# Water volume to serve in a Day
+        self.supplied_volume = 0.00	# Currently supplied water volume
+        
+    def set_min_req(self, req):
+        self.min_req = req
+
+    def set_minimum_water_requirement(self, volume): # ltr / per day, per sq.ft
+    	self.volume_to_supply = volume * self.size
