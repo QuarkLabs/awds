a8da9876ce772cd7d6eaa5b12322966ce9b9ea81
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-02-17 16:15:49 +0530

    Modify functions

--- FlowController/crop.py
+++ FlowController/crop.py
@@ -1,10 +1,10 @@
 class Crop:
     def __init__(self, name, size):
-        self.name = name
-        self.size = size			# Size of the crop field in sq.ft.
-        self.state = "ACTIVE"		# ACTIVE FINISHED CANCELLED
+        self.name = name                # Name of the Crop
+        self.size = size			    # Size of the crop field in sq.ft.
+        self.state = "ACTIVE"		    # ACTIVE FINISHED CANCELLED
         self.volume_to_supply = 0.00	# Water volume to serve in a Day
-        self.supplied_volume = 0.00	# Currently supplied water volume
+        self.supplied_volume = 0.00	    # Supplied water volume
         
     def set_min_req(self, req):
         self.min_req = req
