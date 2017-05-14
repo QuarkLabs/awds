7933c71ce7b49afe3a66c612c96b731f514c5ce1
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-05-14 19:14:23 +0530

    Moisture input.

--- IrrigationAssistant/main.py
+++ IrrigationAssistant/main.py
@@ -19,6 +19,22 @@ def calculate_req_water(type, age, temperature, shower, moisture):
     return [int(min_prediction), int(rec_prediction)]
 
 
+def get_water(type, moisture):
+    condition = update_crop_condition(type, moisture)
+    water_volume = calculate_req_water(condition[0], condition[1], condition[2], condition[3], condition[4])
+    update_water_volume(water_volume)
+    return water_volume[1]     # Return only recommended water amount
+
+
+def update_crop_condition(type, moisture):
+    # Insert moisture to database
+    return [type, 2, 25, 4, moisture]   # type, age, temperature, shower, moisture
+
+
+def update_water_volume(water_volume):
+    pass
+
+
 if __name__ == '__main__':
     # Copy the generated data and model from LRModelTrainer
     file_handler.update_files()
@@ -30,4 +46,10 @@ if __name__ == '__main__':
     # print calculate_req_water(1, 1, 25, 4, 8)
     # print calculate_req_water(1, 1, 25, 4, 9)
     # print calculate_req_water(1, 1, 25, 4, 10)
+
+    print get_water(1, 5)
+    print get_water(1, 10)
+
+
+
 
