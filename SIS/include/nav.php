91e9cd588d13deb8fcb5a177ec9372697d55338c
Kavinda Jayakody <kavindajayakody@gmail.com>
2017-05-15 13:55:25 +0530

    PHP shapeshifted

--- SIS/include/nav.php
+++ SIS/include/nav.php
@@ -15,28 +15,6 @@
                     <div class="title">Dashboard</div>
                 </a>
             </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <div class="icon">
-                        <i class="fa fa-file-o" aria-hidden="true"></i>
-                    </div>
-                    <div class="title">Pages</div>
-                </a>
-                <div class="dropdown-menu">
-                    <ul>
-                        <li class="section"><i class="fa fa-file-o" aria-hidden="true"></i> Admin</li>
-                        <li><a href="./pages/form.html">Form</a></li>
-                        <li><a href="./pages/profile.html">Profile</a></li>
-                        <li><a href="./pages/search.html">Search</a></li>
-                        <li class="line"></li>
-                        <li class="section"><i class="fa fa-file-o" aria-hidden="true"></i> Landing</li>
-                        <!-- <li><a href="./pages/landing.html">Landing</a></li> -->
-                        <li><a href="./pages/login.html">Login</a></li>
-                        <li><a href="./pages/register.html">Register</a></li>
-                         <li><a href="./pages/404.html">404</a></li>
-                    </ul>
-                </div>
-            </li>
         </ul>
     </div>
     <div class="sidebar-footer">
